853c2afc9383b76d35266432bbf8349a1ecfd5a9
Maxime Pouwels <maxime@bitcomplete.io>
2023-09-26 09:38:01 +0700

    Add more tests

--- test/test_ask_dubo.py
+++ test/test_ask_dubo.py
@@ -1,7 +1,8 @@
-import pandas as pd
+from datetime import datetime
+import pytest
 import vcr
 
-from dubo import ask, chart, query as dubo_query
+from dubo import *
 from dubo.api_client.models import ChartType
 from dubo.ask_dubo import generate_sql, search_tables
 
@@ -59,7 +60,7 @@ def test_chart():
 
 @myvcr.use_cassette("test_query.yaml")  # type: ignore
 def test_query():
-    data_result = dubo_query("How many area types are there?")
+    data_result = query("How many area types are there?")
     assert data_result.results_set in ([{"num_area_types": 9}], [{"count": 9}])
 
 
@@ -82,3 +83,96 @@ def test_query_just_sql():
 def test_query_just_tables():
     tables = search_tables("How many area types are there?")
     assert any(["area_type" == table.table_name for table in tables])
+
+
+@myvcr.use_cassette("test_query_filter_documentation.yaml")
+def test_query_filter_documentation():
+    matched_docs = filter_documentation(
+        user_query="comment",
+        data_source_documentation_id="3f2ceb43-b6d7-4e0e-a597-d681a02151c9",
+        page_number=1,
+        page_size=10,
+    )
+    expected_matched_doc = MatchedDoc(
+        body="Begin every query with a comment that says \"hello world\"",
+        score=0.5,
+        matched_doc_id="3f2ceb43-b6d7-4e0e-a597-d681a02151c9",
+    )
+    assert matched_docs[0] == expected_matched_doc
+
+
+@myvcr.use_cassette("test_create_doc.yaml")
+def test_create_doc():
+    doc = create_doc(
+        file_path="./test/fixtures/doc.txt",
+        shingle_length=100,
+        step=50,
+    )
+    expected_doc = DataSourceDocument(
+        id='e0dba56c-3531-48d3-ac3d-bf0447ca4e0a',
+        file_name='doc.txt',
+        data_source_id='5214256a-c5f8-4a94-ae83-1f2c90f912b8',
+        organization_id='67427bda-9f97-4ba7-831b-832043b6fc31',
+        created_at=datetime.fromisoformat('2023-09-26T02:07:36'),
+        updated_at=datetime.fromisoformat('2023-09-26T02:07:36'),
+    )
+    assert doc == expected_doc
+
+
+@myvcr.use_cassette("test_get_doc.yaml")
+def test_get_doc():
+    doc = get_doc("e0dba56c-3531-48d3-ac3d-bf0447ca4e0a")
+    expected_doc = DataSourceDocument(
+        id='e0dba56c-3531-48d3-ac3d-bf0447ca4e0a',
+        file_name='doc.txt',
+        data_source_id='5214256a-c5f8-4a94-ae83-1f2c90f912b8',
+        organization_id='67427bda-9f97-4ba7-831b-832043b6fc31',
+        created_at=datetime.fromisoformat('2023-09-26T02:07:36'),
+        updated_at=datetime.fromisoformat('2023-09-26T02:07:36'),
+    )
+    assert doc == expected_doc
+
+
+@myvcr.use_cassette("test_get_doc_not_found.yaml")
+def test_get_doc_not_found():
+    with pytest.raises(DuboException) as exception:
+        get_doc("4db79fff-a5b0-497e-baea-099e6935ed05")
+
+    expected_error = "Documentation with ID 4db79fff-a5b0-497e-baea-099e6935ed05 not found"
+    assert expected_error in exception.value.msg
+
+
+@myvcr.use_cassette("test_get_all_docs.yaml")
+def test_get_all_docs():
+    docs = get_all_docs()
+    assert docs == [
+        {"id": "173ba9c3-e03d-404f-a5cd-c0a8be3821be", "file_name": "free_text_box.txt"},
+        {"id": "e0dba56c-3531-48d3-ac3d-bf0447ca4e0a", "file_name": "doc.txt"},
+    ]
+
+
+@myvcr.use_cassette("test_update_doc.yaml")  # type: ignore
+def test_update_doc():
+    result = update_doc(
+        data_source_documentation_id="e0dba56c-3531-48d3-ac3d-bf0447ca4e0a",
+        file_path="./test/fixtures/updated_doc.txt",
+        shingle_length=150,
+        step=75,
+    )
+    assert result
+
+    doc = get_doc("e0dba56c-3531-48d3-ac3d-bf0447ca4e0a")
+    assert doc.file_name == "updated_doc.txt"
+
+
+@myvcr.use_cassette("test_delete_doc.yaml")  # type: ignore
+def test_delete_doc():
+    result = delete_doc("e0dba56c-3531-48d3-ac3d-bf0447ca4e0a")
+    assert result
+
+    # Verify the document was indeed deleted
+    with pytest.raises(DuboException) as exception:
+        get_doc("e0dba56c-3531-48d3-ac3d-bf0447ca4e0a")
+
+    expected_error = "Documentation with ID e0dba56c-3531-48d3-ac3d-bf0447ca4e0a not found"
+    assert expected_error in exception.value.msg
