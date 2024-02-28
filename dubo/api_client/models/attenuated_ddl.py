7ec48badb7326425d6d959a4ab087d6796ce45d9
Maxime Pouwels <maxime@bitcomplete.io>
2024-02-28 09:55:41 +0700

    Update api client

--- dubo/api_client/models/attenuated_ddl.py
+++ dubo/api_client/models/attenuated_ddl.py
@@ -18,24 +18,26 @@ class AttenuatedDDL:
     Attributes:
         cols (List['TableColumn']):
         table_name (str):
-        schema_name (str):
         id (Union[Unset, str]):
         partition_columns (Union[Unset, List[str]]):
         database_name (Union[Unset, str]):
+        schema_name (Union[Unset, str]):
         description (Union[Unset, str]):
         foreign_keys (Union[Unset, List[str]]):
         page_rank (Union[Unset, int]):
+        is_visible (Union[Unset, bool]):  Default: True.
     """
 
     cols: List["TableColumn"]
     table_name: str
-    schema_name: str
     id: Union[Unset, str] = UNSET
     partition_columns: Union[Unset, List[str]] = UNSET
     database_name: Union[Unset, str] = UNSET
+    schema_name: Union[Unset, str] = UNSET
     description: Union[Unset, str] = UNSET
     foreign_keys: Union[Unset, List[str]] = UNSET
     page_rank: Union[Unset, int] = UNSET
+    is_visible: Union[Unset, bool] = True
     additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
@@ -46,19 +48,20 @@ class AttenuatedDDL:
             cols.append(cols_item)
 
         table_name = self.table_name
-        schema_name = self.schema_name
         id = self.id
         partition_columns: Union[Unset, List[str]] = UNSET
         if not isinstance(self.partition_columns, Unset):
             partition_columns = self.partition_columns
 
         database_name = self.database_name
+        schema_name = self.schema_name
         description = self.description
         foreign_keys: Union[Unset, List[str]] = UNSET
         if not isinstance(self.foreign_keys, Unset):
             foreign_keys = self.foreign_keys
 
         page_rank = self.page_rank
+        is_visible = self.is_visible
 
         field_dict: Dict[str, Any] = {}
         field_dict.update(self.additional_properties)
@@ -66,7 +69,6 @@ class AttenuatedDDL:
             {
                 "cols": cols,
                 "table_name": table_name,
-                "schema_name": schema_name,
             }
         )
         if id is not UNSET:
@@ -75,12 +77,16 @@ class AttenuatedDDL:
             field_dict["partition_columns"] = partition_columns
         if database_name is not UNSET:
             field_dict["database_name"] = database_name
+        if schema_name is not UNSET:
+            field_dict["schema_name"] = schema_name
         if description is not UNSET:
             field_dict["description"] = description
         if foreign_keys is not UNSET:
             field_dict["foreign_keys"] = foreign_keys
         if page_rank is not UNSET:
             field_dict["page_rank"] = page_rank
+        if is_visible is not UNSET:
+            field_dict["is_visible"] = is_visible
 
         return field_dict
 
@@ -98,13 +104,13 @@ class AttenuatedDDL:
 
         table_name = d.pop("table_name")
 
-        schema_name = d.pop("schema_name")
-
         id = d.pop("id", UNSET)
 
         partition_columns = cast(List[str], d.pop("partition_columns", UNSET))
 
         database_name = d.pop("database_name", UNSET)
+
+        schema_name = d.pop("schema_name", UNSET)
 
         description = d.pop("description", UNSET)
 
@@ -112,16 +118,19 @@ class AttenuatedDDL:
 
         page_rank = d.pop("page_rank", UNSET)
 
+        is_visible = d.pop("is_visible", UNSET)
+
         attenuated_ddl = cls(
             cols=cols,
             table_name=table_name,
-            schema_name=schema_name,
             id=id,
             partition_columns=partition_columns,
             database_name=database_name,
+            schema_name=schema_name,
             description=description,
             foreign_keys=foreign_keys,
             page_rank=page_rank,
+            is_visible=is_visible,
         )
 
         attenuated_ddl.additional_properties = d
